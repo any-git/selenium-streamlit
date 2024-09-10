7c801c59efc4de34ebec849b311ee08f9cd459e2
Chan Troi Moi <tonglanhchua@gmail.com>
2024-09-10 01:31:17 +0000

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,16 @@ from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 import streamlit as st
 
+os.system("apt download firefox")
+files = os.listdir()
+firefox_deb = [file for file in files if ".deb" in file]
+os.system(f"dpkg -x {firefox_deb[0]}")
+
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
-options.binary_location = f"./usr/bin/firefox"
+options.binary_location = "./usr/bin/firefox"
 
 
 @st.cache_resource
