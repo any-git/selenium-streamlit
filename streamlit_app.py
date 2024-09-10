46e887eba305d1620fd1203bfb6c7758cb772860
Chan Troi Moi <tonglanhchua@gmail.com>
2024-09-10 01:21:17 +0000

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,20 +4,20 @@ from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 import streamlit as st
 
+options = Options()
+options.add_argument("--headless")
+
 
 @st.cache_resource
-def get_driver(options):
+def get_driver():
     return webdriver.Firefox(
         service=Service(GeckoDriverManager().install()), options=options
     )
 
 
-options = Options()
-options.add_argument("--headless")
-
 url = st.text_input("URL")
 if url:
-    driver = get_driver(options)
+    driver = get_driver()
     driver.get(url)
     st.code(driver.page_source)
     driver.quit()
