dccdb6f0f038047a7481575f0eec3ac2bf009804
Chan Troi Moi <tonglanhchua@gmail.com>
2024-09-10 01:08:02 +0000

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 import streamlit as st
